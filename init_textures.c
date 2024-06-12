2ae89bec3dac24e52da869ff0be0402b35f0b4e2
mbuchs <mael@buchs.fr>
2024-06-12 17:59:08 +0200

    sorry

--- init_textures.c
+++ init_textures.c
@@ -6,7 +6,7 @@
 /*   By: mbuchs <mbuchs@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 19:34:24 by mbuchs            #+#    #+#             */
-/*   Updated: 2024/06/12 17:22:45 by mbuchs           ###   ########.fr       */
+/*   Updated: 2024/06/12 17:58:27 by mbuchs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,46 @@ char *ft_strndup(char *src, int n)
     return (dest);
 }
 
+int rgbtoint(char *line)
+{
+    int r;
+    int g;
+    int b;
+    int i;
+
+    i = 0;
+    r = 0;
+    g = 0;
+    b = 0;
+    while (line[i] && line[i] != ',')
+    {
+        r = r * 10 + line[i] - '0';
+        i++;
+    }
+    i++;
+    while (line[i] && line[i] != ',')
+    {
+        g = g * 10 + line[i] - '0';
+        i++;
+    }
+    i++;
+    while (line[i])
+    {
+        b = b * 10 + line[i] - '0';
+        i++;
+    }
+	printf("f_color = %x \n", r * 256 * 256 + g * 256 + b);
+    return (r * 256 * 256 + g * 256 + b);
+}
+
 void parse_map_lines(t_data *data)
 //TODO: [] replace printf with ft_exit
 {
     int i = 0;
-    int j;
+    // int j;
     while (data->map[i])
     {
-        j = 0;
+        // j = 0;
         if (data->map[i][0] == 'N' && data->map[i][1] == 'O')
         {
             if (data->cub3d->no_path != NULL)
@@ -64,42 +96,49 @@ void parse_map_lines(t_data *data)
         }
         else if (data->map[i][0] == 'F' && data->map[i][1] == ' ')
         {
+            printf("Error\nskibidigyat\n");
             if (data->cub3d->f_color != -1)
                 printf("Error\nFloor color set more than 1 time\n");
-            data->cub3d->f_color = ft_atoi(data->map[i] + 2);
+            data->cub3d->f_color = rgbtoint(data->map[i] + 2);
         }
         else if (data->map[i][0] == 'C' && data->map[i][1] == ' ')
         {
             if (data->cub3d->c_color != -1)
                 printf("Error\nCeiling color set more than 1 time\n");
-            data->cub3d->c_color = ft_atoi(data->map[i] + 2);
+            data->cub3d->c_color = rgbtoint(data->map[i] + 2);
         }
-        else
-            while (data->map[i][j])
-            {
-                if (data->map[i][j] == '1' || data->map[i][j] == '0')
-                    return ;
-                j++;
-            }  
+        // else
+        //     while (data->map[i][j])
+        //     {
+        //         if (data->map[i][j] == '1' || data->map[i][j] == '0')
+        //             return ;
+        //         j++;
+        //     }  
         i++;
     }
 }
 
 void init_mlx_images(t_data *data)
 {
-    data->cub3d->no_img = mlx_xpm_file_to_image
-		(data->cub3d->mlx, data->cub3d->no_path, &data->cub3d->no_img->height, &data->cub3d->no_img->width);
-    data->cub3d->so_img = mlx_xpm_file_to_image
-        (data->cub3d->mlx, data->cub3d->so_path, &data->cub3d->so_img->height, &data->cub3d->so_img->width);
-    data->cub3d->we_img = mlx_xpm_file_to_image
-        (data->cub3d->mlx, data->cub3d->we_path, &data->cub3d->we_img->height, &data->cub3d->we_img->width);
-    data->cub3d->ea_img = mlx_xpm_file_to_image
-        (data->cub3d->mlx, data->cub3d->ea_path, &data->cub3d->ea_img->height, &data->cub3d->ea_img->width);
-    if (data->cub3d->no_img == NULL || data->cub3d->so_img == NULL || data->cub3d->we_img == NULL || data->cub3d->ea_img == NULL)
+    t_cub3d *c3d;
+    
+    c3d = data->cub3d;
+    c3d->no_img->img = mlx_xpm_file_to_image
+		(c3d->mlx, c3d->no_path, &c3d->no_img->height, &c3d->no_img->width);
+    // c3d->so_img->img = mlx_xpm_file_to_image
+    //     (c3d->mlx, c3d->so_path, &c3d->so_img->height, &c3d->so_img->width);
+    // c3d->we_img->img = mlx_xpm_file_to_image
+    //     (c3d->mlx, c3d->we_path, &c3d->we_img->height, &c3d->we_img->width);
+    // c3d->ea_img->img = mlx_xpm_file_to_image
+    //     (c3d->mlx, c3d->ea_path, &c3d->ea_img->height, &c3d->ea_img->width);
+    printf("no_path = %s\n", c3d->no_path);
+    if (c3d->no_img->img == NULL || c3d->so_img == NULL
+        || c3d->we_img == NULL || c3d->ea_img == NULL)
         printf("Error\nTexture file not found\n");
 }
 
 int init_textures(t_data *data)
+//TODO [] ajouter strtrim
 {
     data->cub3d->no_path = NULL;
     data->cub3d->so_path = NULL;
@@ -111,9 +150,10 @@ int init_textures(t_data *data)
     data->cub3d->no_img = malloc(sizeof(t_img));
     data->cub3d->so_img = malloc(sizeof(t_img));
     data->cub3d->we_img = malloc(sizeof(t_img));
-    
     parse_map_lines(data);
-    if (data->cub3d->no_path == NULL || data->cub3d->so_path == NULL || data->cub3d->we_path == NULL || data->cub3d->ea_path == NULL || data->cub3d->f_color == -1 || data->cub3d->c_color == -1)
+    if (data->cub3d->no_path == NULL || data->cub3d->so_path == NULL
+        || data->cub3d->we_path == NULL || data->cub3d->ea_path == NULL
+        || data->cub3d->f_color == -1 || data->cub3d->c_color == -1)
         printf("Error\nMissing texture or color\n");
     init_mlx_images(data);
     return 0;
